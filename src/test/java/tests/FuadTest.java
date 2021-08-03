70bc275bf977370c88648f27115c86b2cdd01b5e
Fuad <fuadsamadov@gmail.com>
2021-08-03 01:55:41 -0400

    added another class and sout from laptop

--- src/test/java/tests/FuadTest.java
+++ src/test/java/tests/FuadTest.java
@@ -3,6 +3,8 @@ package tests;
 public class FuadTest {
 
     public static void main(String[] args) {
-        System.out.println("Hi World");
+
+        System.out.println("Hi World   " );
+        System.out.println("Hi World 2 ");
     }
 }
---
+++ src/test/java/tests/LaptopTests.java
@@ -0,0 +1,11 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class LaptopTests {
+
+    @Test
+    public void test (){
+        System.out.println("Added laptop side");
+    }
+}
